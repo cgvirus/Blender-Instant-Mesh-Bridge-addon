f8ad65803f55ffb5a810596b3f06668687a7f87d
Fahad Hasan Pathik <contact.fahadpathik@gmail.com>
2023-04-01 07:07:28 +0600

    Modal Menu

--- Instant_Meshes_Remesh.py
+++ Instant_Meshes_Remesh.py
@@ -66,7 +66,7 @@ class InstantMeshesRemesh(bpy.types.Operator):
     verts: bpy.props.IntProperty(name="Vertex Count", description="Desired vertex count of the output mesh", default=2000, min=10, max=50000)
     smooth: bpy.props.IntProperty(name="Smooth iterations", description="Number of smoothing & ray tracing reprojection steps (default: 2)", default=2, min=0, max=10)
     showwire: bpy.props.BoolProperty(name="Show Wireframes", description="Output with wireframe on", default=False)
-    temp_objs_to_proj_dirc: bpy.props.BoolProperty(name="Save temp obj in project directory", description="Save temporary objects in project directory", default=False)
+    # temp_objs_to_proj_dirc: bpy.props.BoolProperty(name="Save temp obj in project directory", description="Save temporary objects in project directory", default=True)
     openUI: bpy.props.BoolProperty(name="Open in InstantMeshes", description="Opens the selected object in Instant Meshes and imports the result when you are done.", default=False)
     # remeshIt: bpy.props.BoolProperty(name="Start Remeshing", description="Activating it will start Remesh", default=False)
 
@@ -83,23 +83,22 @@ class InstantMeshesRemesh(bpy.types.Operator):
 
 
 
-        if self.temp_objs_to_proj_dirc:
-        	if bpy.data.is_saved:
-        		dirname = os.path.join(directory,'instantmeshes_temp')
-        		if not os.path.exists(dirname):
-        			os.makedirs(dirname)
-        		else:
-        			pass
-        		orig = os.path.join(dirname,'original.obj')
-        		output = os.path.join(dirname,'out.obj')
-        	else:
-        		self.report({'ERROR'}, 'File not saved!')
-        		return {'CANCELLED'}
+             
+        if bpy.data.is_saved:
+            dirname = os.path.join(directory,'instantmeshes_temp')
+            
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            else:
+                pass
+            
+            orig = os.path.join(dirname,'original.obj')
+            output = os.path.join(dirname,'out.obj')
+            
         else:
-        	orig = os.path.join(tempfile.gettempdir(), 'original.obj')
-        	output = os.path.join(tempfile.gettempdir(), 'out.obj')
+            self.report({'ERROR'}, 'File not saved! Save the blend file first')
+            return {'CANCELLED'}
 
-        # if self.remeshIt:
 
         if not self.exported:
             try:
@@ -195,10 +194,11 @@ class InstantMeshesRemesh(bpy.types.Operator):
         mesh.hide_viewport = True
         imported_mesh.select_set(state=False)
         os.remove(output)
+        
         if self.showwire:
-        	bpy.context.space_data.overlay.show_wireframes = True
+            bpy.context.space_data.overlay.show_wireframes = True
         else:
-        	bpy.context.space_data.overlay.show_wireframes = False
+            bpy.context.space_data.overlay.show_wireframes = False
 
         return {'FINISHED'}
         
